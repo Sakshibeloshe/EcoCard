c19dab0c117329895ef46f1d8590c0c0063b28b7
Sakshibeloshe <sakshi.beloshe04@gmail.com>
2026-02-27 14:46:10 +0530

    what changes?1

--- Package.swift
+++ Package.swift
@@ -20,7 +20,7 @@ let package = Package(
             teamIdentifier: "M693R36JPD",
             displayVersion: "1.0",
             bundleVersion: "1",
-            appIcon: .placeholder(icon: .note),
+            appIcon: .asset("AppIcon"),
             accentColor: .presetColor(.pink),
             supportedDeviceFamilies: [
                 .pad,
